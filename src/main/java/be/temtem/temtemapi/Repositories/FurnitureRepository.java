6f6aed7ba0a1afc52208f9b0f5ae8480eb3d1119
Korakk8 <oscarlopezluna@gmail.com>
2018-12-07 00:50:18 +0100

    Added @RepositoryRestResource to all repositories

--- src/main/java/be/temtem/temtemapi/Repositories/FurnitureRepository.java
+++ src/main/java/be/temtem/temtemapi/Repositories/FurnitureRepository.java
@@ -2,6 +2,8 @@ package be.temtem.temtemapi.Repositories;
 
 import be.temtem.temtemapi.Entities.Furniture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface FurnitureRepository extends JpaRepository<Furniture, Long> {
 }
--- src/main/java/be/temtem/temtemapi/Repositories/TemtemRepository.java
+++ src/main/java/be/temtem/temtemapi/Repositories/TemtemRepository.java
@@ -2,7 +2,9 @@ package be.temtem.temtemapi.Repositories;
 
 import be.temtem.temtemapi.Entities.Temtem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface TemtemRepository extends JpaRepository<Temtem, Long> {
 
 }
