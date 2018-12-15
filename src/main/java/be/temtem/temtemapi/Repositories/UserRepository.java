e3a426bce2670f275212b48eaa6d0e1b7116f3c8
Korakk8 <oscarlopezluna@gmail.com>
2018-12-15 13:10:56 +0100

    User domain and repo created, also created UriEntity generator and Config file --> UserBasicsService

---
+++ src/main/java/be/temtem/temtemapi/Repositories/UserRepository.java
@@ -0,0 +1,24 @@
+package be.temtem.temtemapi.Repositories;
+
+import be.temtem.temtemapi.Entities.User;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(exported = false)
+public interface UserRepository extends PagingAndSortingRepository<User, String> {
+
+        /**
+         * Finds user by given email with given name as parameter
+         * @param email String email for finding user
+         * @return Returns user object.
+         */
+        User findByEmail(String email);
+
+
+        /**
+         * Finds user by given username, with given name as parameter
+         * @param text String text for finding user
+         * @return Returns user object.
+         */
+        User findByUsername(String text);
+}
